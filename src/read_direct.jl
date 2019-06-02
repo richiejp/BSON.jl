e0699eaa07b00640efdd63765889127271da8ba3
Richard Palethorpe <richiejp@f-m.fm>
2019-06-02 11:01:52 +0200

    Generate parser for known concrete types
    When we know the type in advance we can generate a method just to parse that
    structure. This avoids some work and the compiler gets more type information.

--- src/read_direct.jl
+++ src/read_direct.jl
@@ -1,3 +1,17 @@
+macro asserteq(x, y)
+  xs = sprint(print, x)
+  ys = sprint(print, y)
+
+  quote
+    xv = $(esc(x))
+    yv = $(esc(y))
+    if xv ≠ yv
+      msg = sprint(print, "(", $xs, " = ", xv, ") ≠ (", $ys, " = ", yv, ")")
+      throw(AssertionError(msg))
+    end
+  end
+end
+
 struct BSONElem
   tag::BSONType
   pos::Int
@@ -19,23 +33,38 @@ struct ParseArrayIter{IOT <: IO, ET}
   len::Int
 end
 
+ParseArrayIter(io::IOT, ::Type{ET}, len, ctx) where {IOT, ET} =
+  ParseArrayIter{IOT, ET}(io, ctx, len)
+
 ParseArrayIter(io::IOT, ::Type{ET}, ctx) where {IOT, ET} =
   ParseArrayIter{IOT, ET}(io, ctx, Int(read(io, Int32)) - 4)
 
 Base.iterate(itr::ParseArrayIter) = iterate(itr, 0)
 
-function Base.iterate(itr::ParseArrayIter{IOT, E}, len::Int) where {IOT, E}
+function Base.iterate(itr::ParseArrayIter{IOT, E},
+                      len::Int)::Union{Nothing, Tuple{E, Int}} where {IOT, E}
   startpos = position(itr.io)
   tag = read(itr.io, BSONType)
   if tag == eof
-    @assert len + sizeof(BSONType) == itr.len
+    @asserteq (len + sizeof(BSONType)) itr.len
     return nothing
   end
 
   while read(itr.io, UInt8) != 0x00 end
 
-  obj = parse_tag(itr.io, tag, itr.ctx)::E
+  obj = parse_specific(itr.io, E, tag, itr.ctx)
   (obj, len + position(itr.io) - startpos)
+end
+
+parse_array_len(io::IO, ctx::ParseCtx) = Int(read(io, Int32)) - 4
+
+function parse_array_tag(io::IO, ctx::ParseCtx)::BSONType
+  tag = read(io, BSONType)
+  tag == eof && return eof
+
+  while read(io, UInt8) ≠ 0x00 end
+
+  tag
 end
 
 Base.isempty(itr::ParseArrayIter) = itr.len == 1
@@ -54,7 +83,7 @@ function skip_over(io::IO, tag::BSONType)
   end
 
   seek(io, position(io) + len)
-  #@info "Skipped" tag len position(io)
+#  @info "Skipped" tag len position(io)
 end
 
 "Create an index into the _backrefs entry in the root document"
@@ -94,7 +123,7 @@ end
 
 function setref(obj, ctx::ParseCtx)
   if ctx.curref ≠ -1
-    @assert ctx.refs[ctx.curref] == nothing
+    @asserteq ctx.refs[ctx.curref] nothing
     ctx.refs[ctx.curref] = obj
     ctx.curref = -1
   end
@@ -110,13 +139,13 @@ end
 
 function parse_tag(io::IO, tag::BSONType, ctx::ParseCtx)
   if tag == null
-    @assert ctx.curref == -1
+    @asserteq ctx.curref -1
   elseif tag == document
     parse_doc(io, ctx)
   elseif tag == array
     parse_any_array(io, ctx)
   elseif tag == string
-    @assert ctx.curref == -1
+    @asserteq ctx.curref -1
     len = read(io, Int32)-1
     s = String(read(io, len))
     eof = read(io, 1)
@@ -124,14 +153,213 @@ function parse_tag(io::IO, tag::BSONType, ctx::ParseCtx)
   elseif tag == binary
     parse_bin(io, ctx)
   else
-    @assert ctx.curref == -1
+    @asserteq ctx.curref -1
     read(io, jtype(tag))
   end
 end
 
+for (T, expected) in (Nothing => null,
+                      Bool => boolean,
+                      Int32 => int32,
+                      Int64 => int64,
+                      Float64 => double)
+  @eval begin
+    function parse_specific(io::IO, ::Type{$T}, tag::BSONType,
+                            ctx::ParseCtx)::$T
+      @asserteq tag $expected
+      read(io, $T)
+    end
+  end
+end
+
+function parse_specific(io::IO, ::Type{String},
+                        tag::BSONType, ctx::ParseCtx)::String
+  @asserteq tag string
+  len = read(io, Int32)-1
+  s = String(read(io, len))
+  eof = read(io, 1)
+  #@info "Parse specific string" s
+  s
+end
+
+function parse_specific(io::IO, ::Type{Vector{UInt8}},
+                        tag::BSONType, ctx::ParseCtx)::Vector{UInt8}
+  len = read(io, Int32)
+  subtype = read(io, 1)
+  read(io, len)
+end
+
+function parse_specific(io::IO, ::Type{BSONArray},
+                        tag::BSONType, ctx::ParseCtx)::BSONArray
+  @asserteq tag array
+  parse_any_array(io, ctx)
+end
+
+function parse_specific(io::IO, ::Type{BSONDict},
+                        tag::BSONType, ctx::ParseCtx)::BSONDict
+  @asserteq tag document
+  parse_any_doc(io, ctx)
+end
+
+function parse_specific(io::IO, ::Type{T}, tag::BSONType,
+                        ctx::ParseCtx)::T where T
+  @asserteq tag document
+  #@info "Parse specfic" T
+
+  if @generated
+    if !isconcretetype(T)
+      return :(parse_tag(io, tag, ctx))
+    end
+
+    expr = quote
+      startpos = position(io)
+      len = read(io, Int32)
+      local data::BSONElem
+      ref = nothing
+
+      for _ in 1:4
+        if (tag = read(io, BSONType)) == eof
+          break
+        end
+
+        k = parse_cstr_unsafe(io)
+        if k == b"tag"
+          @asserteq tag string
+        elseif k == b"ref"
+          ref = BSONElem(tag, io)
+        elseif k == b"type"
+          @asserteq tag document
+        elseif k == b"data"
+          @asserteq tag array
+          data = BSONElem(tag, io)
+        end
+
+        skip_over(io, tag)
+      end
+
+      endpos = position(io)
+      @asserteq (startpos + len) endpos
+
+      if ref ≠ nothing
+        ret = parse_specific_ref(io, $T, ref, ctx)
+        seek(io, endpos)
+        return ret
+      end
+
+      seek(io, data.pos)
+    end
+
+    expr = if isprimitive(T)
+      @assert isbitstype(T)
+      quote
+        $expr
+        @asserteq data.tag binary
+        bits = parse_bin(io, ctx)
+        ret = ccall(:jl_new_bits, Any, (Any, Ptr{Cvoid}), $T, bits)
+      end
+    elseif T <: Dict
+      :($expr; ret = load_dict!(io, $T(), ctx))
+    elseif fieldcount(T) < 1
+      :($expr; ret = $T())
+    else
+      quote
+        $expr
+        ret = load_struct!(io, ccall(:jl_new_struct_uninit, Any, (Any,), $T), ctx)
+      end
+    end
+
+    :($expr;
+      seek(io, endpos);
+      #@info "Loaded" $T;
+      ret)
+  else
+    parse_tag(io::IO, tag, ctx)
+  end
+end
+
+function parse_specific(io::IO, ::Type{Array{T, N}}, tag::BSONType,
+                        ctx::ParseCtx)::Array{T, N} where {T, N}
+  if @generated
+    expr = quote
+      startpos = position(io)
+      len = read(io, Int32)
+      local data::BSONElem, sizes
+
+      if tag == array
+        @asserteq $N 1
+        return $T[ParseArrayIter(io, $T, len - 4, ctx)...]
+      end
+      @asserteq tag document
+
+      for _ in 1:5
+        if (tag = read(io, BSONType)) == eof
+          break
+        end
+
+        k = parse_cstr_unsafe(io)
+        if k == b"tag"
+          @asserteq tag string
+          skip_over(io, tag)
+        elseif k == b"type"
+          @asserteq tag document
+          skip_over(io, tag)
+        elseif k == b"size"
+          @asserteq tag array
+          sizes = (ParseArrayIter(io, Int64, ctx)...,)
+        elseif k == b"data"
+          @assert tag == array || tag == binary
+          data = BSONElem(tag, io)
+          skip_over(io, tag)
+        end
+      end
+
+      endpos = position(io)
+      @asserteq (startpos + len) endpos
+      @asserteq length(sizes) $N
+      seek(io, data.pos)
+    end
+
+    if isbitstype(T)
+      :($expr; load_bits_array(io, $T, sizes, data, ctx))
+    else
+      :($expr; load_array(io, $T, sizes, ctx))
+    end
+  else
+    parse_doc(io, ctx)
+  end
+end
+
+function parse_type(io::IO, ctx::ParseCtx)::Type
+  len = read(io, Int32)
+  local params, name
+
+  for _ in 1:3
+    if (tag = read(io, BSONType)) == eof
+      break
+    end
+
+    if k == b"tag"
+      @asserteq tag string
+      skip_over(io, tag)
+    elseif k == "name"
+      name = BSONElem(tag, io)
+      skip_over(io, tag)
+    elseif k == "params"
+      params = BSONElem(tag, io)
+      skip_over(io, tag)
+    end
+  end
+  endpos = position(io)
+
+  T = parse_type(io, name, params, ctx)
+  seek(io, endpos)
+  T
+end
+
 function parse_type(io::IO,
                     name::BSONElem, params::BSONElem, ctx::ParseCtx)::Type
-  @assert name.tag == array && params.tag == array
+  @asserteq name.tag array
+  @asserteq params.tag array
   curref = ctx.curref
   ctx.curref = -1
 
@@ -166,7 +394,7 @@ function load_bits_array(io::IO, ::Type{T}, sizes,
   arr = if sizeof(T) == 0
       fill(T(), sizes...)
     else
-      @assert data.tag == binary
+      @asserteq data.tag binary
       reshape(T[reinterpret(T, parse_bin(io, ctx))...], sizes...)
     end
 
@@ -213,7 +441,8 @@ function parse_array(io::IO, ttype::BSONElem, size::BSONElem,
   end
 end
 
-function parse_backref(io::IO, ref::BSONElem, ctx::ParseCtx)
+function parse_specific_ref(io::IO, ::Type{T}, ref::BSONElem,
+                            ctx::ParseCtx)::T where T
   seek(io, ref.pos)
   id = if ref.tag == int64
     convert(Int32, read(io, Int64))
@@ -229,35 +458,65 @@ function parse_backref(io::IO, ref::BSONElem, ctx::ParseCtx)
     ctx.curref = id
     obj = ctx.refindx[id]
     seek(io, obj.pos)
-    ctx.refs[id] = parse_tag(io, obj.tag, ctx)
+    ctx.refs[id] = parse_specific(io, T, obj.tag, ctx)
   end
+end
+
+function parse_backref(io::IO, ref::BSONElem, ctx::ParseCtx)
+  parse_specific_ref(io, Any, ref, ctx)
 end
 
 function load_dict!(io::IOT, d::Dict{K, V},
                     ctx::ParseCtx) where {K, V, IOT <: IO}
   setref(d, ctx)
 
-  kvs = ParseArrayIter(io, Any, ctx)
-  ks::ParseArrayIter{IOT, K}, vs::ParseArrayIter{IOT, V} = (kvs...,)
-  for (k, v) in zip(kvs...)
+  parse_array_len(io, ctx)
+  tag = parse_array_tag(io, ctx)
+  ks = parse_specific(io, Vector{K}, tag, ctx)
+
+  tag = parse_array_tag(io, ctx)
+  vs = parse_specific(io, Vector{V}, tag, ctx)
+
+  for (k, v) in zip(ks, vs)
       d[k] = v
   end
 
   d
 end
 
-function load_struct!(io::IO, x::T, ctx::ParseCtx) where T
+function load_struct!(io::IO, x::T, ctx::ParseCtx)::T where T
   setref(x, ctx)
+  #@info "Load struct" T
 
-  local p
-  for outer p = enumerate(ParseArrayIter(io, Any, ctx))
-    (i, field) = p
-    f = convert(fieldtype(T, i), field)
-    ccall(:jl_set_nth_field, Nothing, (Any, Csize_t, Any), x, i-1, f)
+  parse_array_len(io, ctx)
+
+  if @generated
+    n = fieldcount(T)
+    @assert n > 0
+    FT = fieldtype(T, 1)
+
+    block = :(tag = parse_array_tag(io, ctx);
+              f = parse_specific(io, $FT, tag, ctx);
+              ccall(:jl_set_nth_field, Nothing, (Any, Csize_t, Any), x, 0, f))
+    for i in 2:n
+      FT = fieldtype(T, i)
+      block = :($block;
+                tag = parse_array_tag(io, ctx);
+                f = parse_specific(io, $FT, tag, ctx);
+                ccall(:jl_set_nth_field, Nothing, (Any, Csize_t, Any), x, $i-1, f))
+    end
+
+    :($block; x)
+  else
+    for i in 1:nfields(x)
+      tag = parse_array_tag(io, ctx)
+      field = parse_tag(io, tag, ctx)
+      f = convert(fieldtype(T, i), field)
+      ccall(:jl_set_nth_field, Nothing, (Any, Csize_t, Any), x, i-1, f)
+    end
+
+    x
   end
-
-  @assert p[1] == fieldcount(T) "$(p[1]) ≠ $(fieldcount(T))"
-  x
 end
 
 function parse_struct(io::IO, ttype::BSONElem, data::BSONElem, ctx::ParseCtx)
@@ -266,7 +525,7 @@ function parse_struct(io::IO, ttype::BSONElem, data::BSONElem, ctx::ParseCtx)
   ctx.curref = -1
 
   seek(io, ttype.pos)
-  @assert ttype.tag == document
+  @asserteq ttype.tag document
   T = parse_doc(io, ctx)::Type
   #@info "New struct type" T
 
@@ -275,13 +534,15 @@ function parse_struct(io::IO, ttype::BSONElem, data::BSONElem, ctx::ParseCtx)
 
   if isprimitive(T)
     @assert isbitstype(T)
-    @assert data.tag == binary
+    @asserteq data.tag binary
     bits = parse_bin(io, ctx)
     ccall(:jl_new_bits, Any, (Any, Ptr{Cvoid}), T, bits)
   elseif T <: Dict
     load_dict!(io, T(), ctx)
+  elseif fieldcount(T) < 1
+    T()
   else
-    @assert data.tag == array
+    @asserteq data.tag array
     load_struct!(io, ccall(:jl_new_struct_uninit, Any, (Any,), T), ctx)
   end
 end
@@ -385,7 +646,7 @@ function parse_doc(io::IO, ctx::ParseCtx)
     seek(io, tname.pos)
     len = read(io, Int32)-1
     cstr = parse_cstr_unsafe(io)
-    @assert len == length(cstr)
+    @asserteq len length(cstr)
     ccall(:jl_symbol_n, Symbol, (Ptr{UInt8}, Int), cstr, len)
   elseif only_saw(SEEN_TAG | SEEN_DATA | SEEN_TAG_TUPLE)
     #@info "Found Tuple" tdata
@@ -432,7 +693,7 @@ function directtrip(ting::T) where {T}
   ctx = ParseCtx()
   build_refs_indx!(io, ctx)
   try
-    parse_doc(io, ctx)[:stuff]
+    parse_doc(io, ctx)[:stuff]::T
   catch e
     @error "Error during parsing" io ctx
     rethrow(e)
